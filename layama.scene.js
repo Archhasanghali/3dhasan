c2943d12ebc2269575341e11e52c8f2af2627797
Archhasanghali <arch.hasanghali@gmail.com>
2025-09-17 17:26:58 +0200

    Add files via upload

--- layama.scene.js
+++ layama.scene.js
@@ -4,17 +4,17 @@
 function getLayamaCameras()
 {
    var layamaCameras = [];
-   layamaCameras.push({n: "Layama0002", a: "CineCameraActor1", p: {x:5937.01, y:185.757, z:927.348}, l: {x:5937.02, y:185.681, z:926.351},f: [5784.1,0,1,0],c: [1,1,1,1]});
-   layamaCameras.push({n: "Layama0009", a: "CineCameraActor", p: {x:2105.46, y:172.539, z:804.724}, l: {x:2106.46, y:172.539, z:804.724}, f: [5770.89,0,1,0], c: [1,1,1,1]});
-   layamaCameras.push({n: "Layama0016", a: "CineCameraActor2", p: {x:2105.46, y:172.539, z:550.889}, l: {x:2106.46, y:172.539, z:550.889}, f: [5770.89,0,1,0], c: [1,1,1,1]});
-   layamaCameras.push({n: "Layama0023", a: "CineCameraActor3", p: {x:2342.02, y:172.539, z:550.889}, l: {x:2343.02, y:172.539, z:550.889}, f: [5770.89,0,1,0], c: [1,1,1,1]});
+   layamaCameras.push({n: "Layama0002", a: "CineCameraActor1", p: {x:259.804, y:605.69, z:276.977}, l: {x:260.804, y:605.722, z:276.949},f: [3985.28,0,1,0],c: [1,1,1,1]});
+   layamaCameras.push({n: "Layama0009", a: "CineCameraActor", p: {x:1102.76, y:555.515, z:291.733}, l: {x:1103.76, y:555.515, z:291.733}, f: [3935.11,0,1,0], c: [1,1,1,1]});
+   layamaCameras.push({n: "Layama0016", a: "CineCameraActor2", p: {x:1840.72, y:555.515, z:189.02}, l: {x:1841.72, y:555.515, z:189.02}, f: [3935.11,0,1,0], c: [1,1,1,1]});
+   layamaCameras.push({n: "Layama0023", a: "CineCameraActor3", p: {x:2877.37, y:555.515, z:498.996}, l: {x:2878.37, y:555.515, z:498.996}, f: [3935.11,0,1,0], c: [1,1,1,1]});
    return layamaCameras;
 }
 
 function getLayamaResolutions()
 {
    var layamaResolutions = [];
-   layamaResolutions.push("4096");
+   layamaResolutions.push("2048");
    layamaResolutions.push("1024");
    return layamaResolutions;
 }
